7860a5bda6c0456b5bd28a8910e85b4140697bb9
Matthew Mielnik <64508655+MatthewMielnik@users.noreply.github.com>
2020-05-17 16:13:49 -0400

    Update mazeProject_app.js

--- js/mazeProject_app.js
+++ js/mazeProject_app.js
@@ -1,306 +1,256 @@
-
-
-function initiateApp(action) {
+document.addEventListener('DOMContentLoaded', function() {
 	
-	switch (action) {
-		case 'setup':
-			setUpMaze();
-			break;
-		case 'play':
-			playMaze();
-			break;
-		}
+	// set header; build foundational structure
+	document.body.appendChild(document.createElement('div')).id = 'header';
+	header.appendChild(document.createElement('h2')).innerHTML = "A-maze Game";
+	header.appendChild(document.createElement('p')).innerHTML = "Play with a friend ...or your dog, if they're really smart.";
 	
-}
+	header.appendChild(document.createElement('div')).id = 'startButton';
+	startButton.appendChild(document.createElement('p'));
+	startButton.querySelector('p').innerHTML = "Let's Go!";
+	startButton.addEventListener( 'click', setUpMaze );
+	
+	document.body.appendChild(document.createElement('div')).id = 'appContainer';
+	appContainer.appendChild(document.createElement('div')).id = 'app';
+	
+})
+
 
 function setUpMaze() {
 
+	// set header
 	header.querySelector('p').innerHTML = ("Whoever is the Riddler... build your trap.<br>"+
 												"Build or remove walls of your maze by selecting the squares.<br>"+
 												"Double click a square to choose destination of your maze – the winner spot.");
 	
 	startButton.querySelector('p').innerHTML = "You're on!";
-	startButton.setAttribute('onclick', 'initiateApp("play")');
-												
-	createMaze();
+	startButton.removeEventListener( 'click', setUpMaze );
+	startButton.addEventListener( 'click', playMaze );
+
+
+	// build base of the maze
+	for (let x = 0; x < 225; x++) { 
+
+		// make mazeBlocks
+		const mazeBlock = document.createElement('div');
+		mazeBlock.className = 'mazeBlock mazeBlockSetup';
+		app.appendChild(mazeBlock);
+		mazeBlock.id = mazeBlock.offsetTop.toString() + mazeBlock.offsetLeft.toString();
 	
+		// identify player start block, show info; attach maze building algos to remaining blocks
+		if (mazeBlock.id != '0140') {
+			mazeBlock.addEventListener( 'click', buildWalls );
+			mazeBlock.addEventListener( 'dblclick', setGoal );
+		} else {
+			mazeBlock.classList.add('nogo');
+			var infoBox = mazeBlock.appendChild(document.createElement('p'));
+			infoBox.innerHTML = "The player starts here...";
+			mazeBlock.addEventListener( 'mouseover', function(){ infoBox.style.display = 'inline' } );
+			mazeBlock.addEventListener( 'mouseout', function(){ infoBox.style.display = 'none' } );
+		}
+		
+	}
+	
+
+	/*==================================================================================
+	function getRandom(arr, n) {
+	    var result = new Array(n),
+	        len = arr.length,
+	        taken = new Array(len);
+	    if (n > len)
+	        throw new RangeError("getRandom: more elements taken than available");
+	    while (n--) {
+	        var x = Math.floor(Math.random() * len);
+	        result[n] = arr[x in taken ? taken[x] : x];
+	        taken[x] = --len in taken ? taken[len] : len;
+	    }
+	    return result;
+	}
+		
+	(function() {
+		let randBlocks = getRandom(document.getElementsByClassName('mazeBlock'),51);
+		for (let x = 0; x < randBlocks.length; x++) { 
+			let block = randBlocks[x];
+			if (block.id != '0140' || block.id != '0160') { block.classList.add('wall') };
+		}
+	})();
+	*/// ==================================================================================
+
+
+	// setup keyboard cues
+	app.appendChild(document.createElement('div')).id = 'cue';
+	cue.appendChild(document.createElement('p')).id = 'arrow';
+	arrow.innerHTML = "&#10140;";
+
 }
 
-	function createMaze() {
-
-		var x;	
-		for (x = 0; x < 225; x++) { 
-
-			// make mazeBlocks
-			var mazeBlock = document.createElement('div');
-			mazeBlock.setAttribute('class', 'mazeBlock');
-			app.appendChild(mazeBlock);
-			mazeBlock.setAttribute( 'id', mazeBlock.offsetTop.toString() + mazeBlock.offsetLeft.toString() );
-		
-			// identify player start block, show related info; attach maze building algos to remaining blocks
-			if (mazeBlock.id !== '0140') {
-				mazeBlock.addEventListener( 'click', buildWalls(this.id) );
-				mazeBlock.setAttribute( 'ondblclick', 'setGoal('+'"'+ mazeBlock.id +'"'+')' );
+		function buildWalls() {
+			if (!this.classList.contains('wall')) {
+				this.classList.add('wall');
 			} else {
-				mazeBlock.classList.add('nogo');
-				var infoBox = mazeBlock.appendChild(document.createElement('p'));
-				infoBox.innerHTML = "The player starts here...";
-				mazeBlock.setAttribute('onmouseover', "(function(e){ e.children[0].style.display = 'inline' })(this)" );
-				mazeBlock.setAttribute('onmouseout', "(function(e){ e.children[0].style.display = 'none' })(this)" );
+				this.classList.remove('wall');
 			}
-			
 		}
 
-	}
-
-		function buildWalls(t) {
-	//alert(t + " : " + typeof t)
-			var targetBlock = document.getElementById(t);
-			if (!targetBlock.classList.contains('wall')) {
-				targetBlock.classList.add('wall');
+		function setGoal() {
+			this.classList.remove('wall');
+			if (!this.classList.contains('prize')) {
+				this.classList.add('prize', 'prizeSetup');
 			} else {
-				targetBlock.classList.remove('wall');
+				this.classList.remove('prize', 'prizeSetup');
 			}
-	
 		}
-
-		function setGoal(t) {
-	
-			var targetBlock = document.getElementById(t);
-			targetBlock.classList.remove('wall');
-			if (!targetBlock.classList.contains('prize')) {
-				targetBlock.classList.add('prize');
-				targetBlock.style.background = '#FDD220';
-			} else {
-				targetBlock.classList.remove('prize');
-				targetBlock.style.background = '';
-			}
-	
-		}
-
-
-
-
-
 
 
 function playMaze() {
 	
-	createMazeGoer();
-	hopMazeGoer();
-	createDirectionCues();
-	document.getElementById('startButton').style.display = 'none';
-	document.getElementById('header').querySelector('p').innerHTML = "Problem solver... it's your turn!<br><font size='2px' color='#a1a9b2'>Use the keyboard arrow keys to find your way out.</font>";
+	// set header
+	startButton.style.display = 'none';
+	header.querySelector('p').innerHTML = ("Problem solver... it's your turn!<br>"+
+												"Use the keyboard arrow keys to find your way out.");
 	
+	// switch maze visuals and event listeneres to game mode
 	document.getElementById('0140').classList.remove('nogo');
 	var blocks = document.getElementsByClassName('mazeBlock');
-	for (var i = 0; i < blocks.length; i++) {
-		var block = blocks[i];
-		block.removeEventListener();
-		block.removeAttribute('ondblclick');
-		block.style.outline = '0px';
-		block.style.background = '';
-	}
-
-}
-
-
-
-
-
-function createMazeGoer() {
-	
-	// mazeGoer
-	var mazeGoer = document.createElement('div');
-	mazeGoer.setAttribute('id', 'mazeGoer');
-	app.appendChild(mazeGoer);
-	
-		// mazeGoer styles
-		var mazeGoerBase = document.createElement('style');
-		mazeGoerBase.type = 'text/css';
-		var mazeGoerBaseStyles = document.createTextNode( '' +
-			'#mazeGoer {' +
-			'  position: absolute;'+
-			'  top: 0px;'+
-			'  left: 140px;'+
-			'  width: 20px;'+
-			'  height: 20px;'+
-			'  border-radius: 10px;'+
-			'  background: rgb(224,255,40);'+
-			'  background: radial-gradient(circle, rgba(255,235,40,1) 0%, rgba(213,171,0,1) 100%);'+
-			'}');
-		mazeGoerBase.appendChild(mazeGoerBaseStyles);
-		mazeGoer.appendChild(mazeGoerBase);
-	
-}
-
-function createDirectionCues() {
-	
-	// cue
-	var cue = document.createElement('div');
-	cue.setAttribute('id', 'cue');
-	app.appendChild(cue);
-	
-		// cue styles
-		var cueBase = document.createElement('style');
-		cueBase.type = 'text/css';
-		var cueBaseStyles = document.createTextNode( '' +
-			'#cue {' +
-			'  position: absolute;'+
-			'  top: 100px;'+
-			'  left: 400px;'+
-			'  width: 50px;'+
-			'  height: 50px;'+
-			'  text-align: center;'+
-			'  padding-top: 0.15em;'+
-			'  opacity: 0;'+
-			'}');
-		cueBase.appendChild(cueBaseStyles);
-		cue.appendChild(cueBase);
-	
-		var cueAnimation = document.createElement('style');
-		cueAnimation.type = 'text/css';
-		var cueAnimationStyles = document.createTextNode( '' +
-			'#cue.anim {' +
-			'  animation: pop 0.5s;'+
-			'}' +
-			'@keyframes pop {'+
-			'  0% { opacity:0; }'+
-			'  50% { opacity:1; }'+
-			'  100% { opacity:0; }'+
-			'}');
-		cueAnimation.appendChild(cueAnimationStyles);
-		cue.appendChild(cueAnimation);
-	
-	
-	// arrow
-	var arrow = document.createElement('span');
-	arrow.setAttribute('id', 'arrow');
-	arrow.innerHTML = "&#10140;";
-	cue.appendChild(arrow);
-
-	
-		// arrow styles
-		var arrowBase = document.createElement('style');
-		arrowBase.type = 'text/css';
-		var arrowBaseStyles = document.createTextNode( '' +
-			'#arrow {' +
-			'  position: relative;'+
-			'  font-size: 3em;'+
-			'  color: #d1d1e0;'+
-			'}');
-		arrowBase.appendChild(arrowBaseStyles);
-		arrow.appendChild(arrowBase);
-		
-}
-
-function hopMazeGoer() {
-	
-	var mazeGoer = document.getElementById('mazeGoer');
-	var hopSize = 20;
-	
-	document.onkeydown = function(event) {
-		switch (event.keyCode) {
-			case 37: // left
-				var wallLocation = document.getElementById( (mazeGoer.offsetTop).toString() + (mazeGoer.offsetLeft - hopSize).toString() );
-				if (!wallLocation.classList.contains('wall')) {
-					mazeGoer.style.left = (mazeGoer.offsetLeft - hopSize) + 'px';
-					if (wallLocation.classList.contains('prize')) { prizeWon() };
-				}
-				hopCue(180);
-				break;
-			case 39: // right
-				var wallLocation = document.getElementById( (mazeGoer.offsetTop).toString() + (mazeGoer.offsetLeft + hopSize).toString() );
-				if (!wallLocation.classList.contains('wall')) {
-					mazeGoer.style.left = (mazeGoer.offsetLeft + hopSize) + 'px';
-					if (wallLocation.classList.contains('prize')) { prizeWon() };
-				}
-				hopCue(0);
-				break;
-			case 38: // up
-				var wallLocation = document.getElementById( (mazeGoer.offsetTop - hopSize).toString() + (mazeGoer.offsetLeft).toString() );
-				if (!wallLocation.classList.contains('wall')) {
-		            mazeGoer.style.top = (mazeGoer.offsetTop - hopSize) + 'px';
-					if (wallLocation.classList.contains('prize')) { prizeWon() };
-				}
-				hopCue(270);
-				break;
-			case 40: // down
-				var wallLocation = document.getElementById( (mazeGoer.offsetTop + hopSize).toString() + (mazeGoer.offsetLeft).toString() );
-				if (!wallLocation.classList.contains('wall')) {
-		            mazeGoer.style.top = (mazeGoer.offsetTop + hopSize) + 'px';
-					if (wallLocation.classList.contains('prize')) { prizeWon() };
-				}
-				hopCue(90);
-				break;
-	    }
+	for (let i = 0; i < blocks.length; i++) {
+		let block = blocks[i];
+		block.removeEventListener( 'click', buildWalls );
+		block.removeEventListener( 'dblclick', setGoal );
+		block.innerHTML = '';
+		block.classList.remove('mazeBlockSetup');
+		block.classList.remove('prizeSetup');
 	}
 	
+	// initiate mazeGoer
+	app.appendChild(document.createElement('div')).id = 'mazeGoer';
+	hopMazeGoer();
+
 }
 
-function prizeWon() {
+		function hopMazeGoer() {
+
+			let hopSize = 20;
+			document.onkeydown = function(event) {
+
+				switch (event.keyCode) {
+					case 37: // left
+						var cD_result = collisionDetector.testHopSpace(event.keyCode, hopSize);
+						if (cD_result != 'wall') {
+							mazeGoer.style.left = (mazeGoer.offsetLeft - hopSize) + 'px';
+							(cD_result == 'prize') ? prizeWon() : null;
+						}
+						hopCue(180);
+						break;
+						
+					case 39: // right
+						var cD_result = collisionDetector.testHopSpace(event.keyCode, hopSize);
+						if (cD_result != 'wall') {
+							mazeGoer.style.left = (mazeGoer.offsetLeft + hopSize) + 'px';
+							(cD_result == 'prize') ? prizeWon() : null;
+						}
+						hopCue(0);
+						break;
+						
+					case 38: // up
+						var cD_result = collisionDetector.testHopSpace(event.keyCode, hopSize);
+						if (cD_result != 'wall') {
+							mazeGoer.style.top = (mazeGoer.offsetTop - hopSize) + 'px';
+							(cD_result == 'prize') ? prizeWon() : null;
+						}
+						hopCue(270);
+						break;
+						
+					case 40: // down
+						var cD_result = collisionDetector.testHopSpace(event.keyCode, hopSize);
+						if (cD_result != 'wall') {
+							mazeGoer.style.top = (mazeGoer.offsetTop + hopSize) + 'px';
+							(cD_result == 'prize') ? prizeWon() : null;
+						}
+						hopCue(90);
+						break;
+						
+			    }
+			}
 	
-	var mazeGoer = document.getElementById('mazeGoer');
+		}
 	
-	setTimeout(function() {
-		mazeGoer.classList.add('winner');
+		var collisionDetector = {
+
+			mazeGoer : document.getElementById('mazeGoer'),
+			testHopSpace : function(directionOfMove, hopSize) {
+				
+				let hopTarget = (function() {
+					switch (directionOfMove) {
+					/* left */	case 37: return document.getElementById( (this.mazeGoer.offsetTop).toString() + (this.mazeGoer.offsetLeft - hopSize).toString() ); break;
+					/* right */ case 39: return document.getElementById( (this.mazeGoer.offsetTop).toString() + (this.mazeGoer.offsetLeft + hopSize).toString() ); break;
+					/* up */	case 38: return document.getElementById( (this.mazeGoer.offsetTop - hopSize).toString() + (this.mazeGoer.offsetLeft).toString() ); break;
+					/* down */	case 40: return document.getElementById( (this.mazeGoer.offsetTop + hopSize).toString() + (this.mazeGoer.offsetLeft).toString() ); break;
+					}
+				})();
+				
+				let cl = hopTarget.classList;
+				switch (true) {
+					/* wall */	case cl.contains('wall')	: return 'wall'; break;
+					/* prize */ case cl.contains('prize')	: return 'prize'; break;
+					/* open */	default						: return null
+				}
+
+  		  	}
+		  
+		}
+
+		function hopCue(rotateAngle) {
+
+			let app = document.getElementById('app');
+			let cue = document.getElementById('cue');
+			app.removeChild(cue);
+			app.appendChild(cue);
+			cue.style.transform = 'rotate(' + rotateAngle + 'deg)';
+			cue.classList.add('cueAnim');
+	
+		}
+
+		function prizeWon() {
+	
+			// exit animations and restart
+			function contentsAnimations() {
+	
+				header.style.opacity = 0;
+				mazeGoer.classList.add('winner');
 		
-		// banner
-		var banner = document.createElement('div');
-		banner.setAttribute('id', 'banner');
-		banner.innerHTML = "Nice Job!";
-		banner.classList.add('anim');
-		banner.style.animationName = 'pop';
-		appContainer.appendChild(banner);
+				appContainer.appendChild(document.createElement('div')).id = 'banner';
+		 		banner.appendChild(document.createElement('p')).innerHTML = "Nice Job!";
+		 		banner.classList.add('bannerAnim');
+
+				var blocks = document.getElementsByClassName('wall');
+				for (let i = 0; i < blocks.length; i++) {
+					let block = blocks[i];
+					block.classList.add('mazeBlockAnim');
+				}
 		
-			// banner styles
-			var bannerBase = document.createElement('style');
-			bannerBase.type = 'text/css';
-			var bannerBaseStyles = document.createTextNode( '' +
-				'#banner {' +
-				'  position: relative;'+
-				'  top: -220px;'+
-				'  text-align: center;'+
-				'  text-align: center;'+
-				'  font-family: Arial, Helvetica, sans-serif;'+
-				'  color: #FDD220;'+
-				'  font-size: 5em;'+
-				'  font-weight: bold;'+
-				'  opacity: 0;'+
-				'}');
-			bannerBase.appendChild(bannerBaseStyles);
-			banner.appendChild(bannerBase);
+			}
+	
+			function appAnimations() {
+				app.classList.add('appAnim');
+			}
+
+			function restartGame() {
+				appContainer.appendChild(document.createElement('div')).id = 'restartButton';
+				restartButton.appendChild(document.createElement('p'));
+				restartButton.querySelector('p').innerHTML = "Let's Go Again!";
+				restartButton.classList.add('rsbAnim');
+				restartButton.addEventListener( 'click', function(){ location.reload() } );
+			}
 			
-			var bannerAnimation = document.createElement('style');
-			bannerAnimation.type = 'text/css';
-			var bannerAnimationStyles = document.createTextNode( '' +
-				'#banner.anim {' +
-				'  animation: pop 0.8s;'+
-				'}' +
-				'@keyframes pop {'+
-				'  0% { opacity:0; transform:scale(1,1); }'+
-				'  50% { opacity:1; transform:scale(2,2); }'+
-				'  100% { opacity:0; transform:scale(1,1); }'+
-				'}');
-			bannerAnimation.appendChild(bannerAnimationStyles);
-			banner.appendChild(bannerAnimation);
-		
-	}, 50);
+			
+			// animation sequencing
+			setTimeout(function() { contentsAnimations(); }, 50);
+			setTimeout(function() { appAnimations(); }, 600);
+			setTimeout(function() { restartGame(); }, 1800);
 	
-}
+		}
 
-function hopCue(rotateAngle) {
-	
-	var app = document.getElementById('app');
-	var cue = document.getElementById('cue');
-	app.removeChild(cue);
-	app.appendChild(cue);
-	cue.style.transform = 'rotate('+rotateAngle+'deg)';
-	cue.style.transformOrigin = '0% 50%';
-	cue.classList.add('anim');
-	cue.style.animationName = 'pop';
-	
-}
+
 
 
 
