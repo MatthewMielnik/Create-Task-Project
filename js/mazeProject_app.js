5f0460aa48a4de26b298eda5c30ddeda7d0b13a5
Matthew Mielnik <64508655+MatthewMielnik@users.noreply.github.com>
2020-05-17 21:08:14 -0400

    Update mazeProject_app.js

--- js/mazeProject_app.js
+++ js/mazeProject_app.js
@@ -37,31 +37,6 @@ function setUpMaze() {
 	cue.appendChild(document.createElement('p')).id = 'arrow';
 	arrow.innerHTML = "&#10140;";
 	
-	
-	/*==================================================================================
-	function getRandom(arr, n) {
-	    var result = new Array(n),
-	        len = arr.length,
-	        taken = new Array(len);
-	    if (n > len)
-	        throw new RangeError("getRandom: more elements taken than available");
-	    while (n--) {
-	        var x = Math.floor(Math.random() * len);
-	        result[n] = arr[x in taken ? taken[x] : x];
-	        taken[x] = --len in taken ? taken[len] : len;
-	    }
-	    return result;
-	}
-		
-	(function() {
-		let randBlocks = getRandom(document.getElementsByClassName('mazeBlock'),51);
-		for (let x = 0; x < randBlocks.length; x++) { 
-			let block = randBlocks[x];
-			if (block.id != '0140' || block.id != '0160') { block.classList.add('wall') };
-		}
-	})();
-	*/// ==================================================================================
-
 }
 
 		var mazeBuilder = {
